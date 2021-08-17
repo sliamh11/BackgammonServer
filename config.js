84adf49b14ddda1820f6a5f81d890124be0bba49
sliamh11 <68326628+sliamh11@users.noreply.github.com>
2021-08-17 15:07:36 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Holds environment data.
-const config = {
-    PORT: 8080,
-    MONGO_URI: "mongodb+srv://sliamh11:curufuc22@cluster0.wozrm.mongodb.net/SelaTalkback?retryWrites=true&w=majority",
-    TOKEN_KEY: "abcdefg"
-};
-
-module.exports = config;
