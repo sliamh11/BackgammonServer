147f27a547bbe92648fcc7de60328fe31b28368b
User <User@DESKTOP-CT4MMP5>
2021-08-17 14:58:43 +0300

    Updated config settings

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Holds environment data.
 const config = {
-    PORT: 8080,
-    MONGO_URI: "mongodb+srv://sliamh11:curufuc22@cluster0.wozrm.mongodb.net/SelaTalkback?retryWrites=true&w=majority",
+    PORT: process.env.API_PORT || 8080,
+    MONGO_URI: process.env.MONGO_URI,
     TOKEN_KEY: "abcdefg"
 };
 
